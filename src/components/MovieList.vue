1d18c18cee9b327aebe10904579042a1fe1d5f69
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 22:08:07 +0200

    added a v-for into the single movie and imported the store

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,16 +1,17 @@
-<template lang="">
-    <SingleMovie/>
+<template>
+    <SingleMovie v-for="movie in store.movieList" :infos="movie"/>
 </template>
 
 
 <script>
 import SingleMovie from './SingleMovie.vue';
+import {store} from '../store.js';
 
 export default {
     name :'MovieList',
     data() {
         return {
-            
+            store
         }
     },
     components:{
