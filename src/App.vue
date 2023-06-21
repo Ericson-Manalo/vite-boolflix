fe87da0e39631c665ee762386e7d00280a55ad4a
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 15:29:14 +0200

    imported bootstrap

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,13 @@ import MainFlix from './components/MainFlix.vue';
   <MainFlix/>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../node_modules/bootstrap/scss/bootstrap';
+
 </style>
+
+
+<!--b4e23a9ed230e66a8e63084cff9d0035-->
+<!--
+  https://api.themoviedb.org/3/search/movie?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=
+-->
