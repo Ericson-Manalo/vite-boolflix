b0a64b1fe0d2172b253a7ce5d029713097134542
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 16:49:56 +0200

    implemented movielist component by importing to it singlemovie component

---
+++ src/components/MovieList.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <SingleMovie/>
+</template>
+
+
+<script>
+import SingleMovie from './SingleMovie.vue';
+
+export default {
+    name :'MovieList',
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+        SingleMovie
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
