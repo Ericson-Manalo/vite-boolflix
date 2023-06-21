a11e01ebe94f4ab8569e12ede37eedacb9a3c1ce
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 15:53:13 +0200

    creating my search bar and testing in it if i can see what i write on console.log with @click

--- src/components/MovieSearchFlix.vue
+++ src/components/MovieSearchFlix.vue
@@ -1,7 +1,9 @@
 <template lang="">
-    <h1>
-        
-    </h1>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Type to search a Movie" aria-label="Username" aria-describedby="basic-addon1" v-model="searchMovie">
+        <button @click="logContent(searchMovie)">Search</button>
+    </div>
+
 </template>
 
 
@@ -10,12 +12,17 @@ export default {
     name :'MovieSearchFlix',
     data() {
         return {
-            
+            searchMovie: ''
         }
     },
     components:{
         
-    }
+    },
+    methods: {
+        logContent(content){
+            console.log(content)
+        }
+    },
 }
 </script>
 <style lang="scss">
