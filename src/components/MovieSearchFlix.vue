99629af2ffae467df917182aff45104510143783
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 21:30:35 +0200

    put the emit at click, imported to this component store

--- src/components/MovieSearchFlix.vue
+++ src/components/MovieSearchFlix.vue
@@ -1,27 +1,26 @@
 <template lang="">
     <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Type to search a Movie" aria-label="Username" aria-describedby="basic-addon1" v-model="searchMovie">
-        <button @click="logContent(searchMovie)">Search</button>
+        <input type="text" class="form-control" placeholder="Type to search a Movie" aria-label="Username" aria-describedby="basic-addon1" v-model="store.searchMovie">
+        <button @click="$emit('searched')">Search</button>
     </div>
 
 </template>
 
 
 <script>
+import {store} from '../store.js';
 export default {
     name :'MovieSearchFlix',
     data() {
         return {
-            searchMovie: ''
+            store
         }
     },
     components:{
         
     },
     methods: {
-        logContent(content){
-            console.log(content)
-        }
+        
     },
 }
 </script>
