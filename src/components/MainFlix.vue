1c2a34fd8f75b10142449459421c2c5748954479
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 21:29:34 +0200

    imported axios link added function that gives the output of user's input

--- src/components/MainFlix.vue
+++ src/components/MainFlix.vue
@@ -2,10 +2,7 @@
     <h1>
         MainFlix
     </h1>
-
-import MovieList from './MovieList.vue';
-
-    <MovieSearchFlix @searched/>
+    <MovieSearchFlix @searched="searchFilm"/>
     <MovieList/>
 
 </template>
@@ -15,20 +12,44 @@ import MovieList from './MovieList.vue';
 import {store} from '../store.js';
 import MovieSearchFlix from './MovieSearchFlix.vue';
 import MovieList from './MovieList.vue';
+import axios from 'axios';
 
 
 export default {
     name:'MainFlix',
     data() {
         return {
-            store
-
+            store,
+            
         }
     },
     components:{
-    MovieSearchFlix,
-    MovieList
-}
+        MovieSearchFlix,
+        MovieList
+    },
+    methods:{
+        searchFilm(){
+            let listMovie = store.apiUrl;
+
+            if(store.searchMovie !== ""){
+                listMovie += `${store.searchMovie}`
+                console.log(listMovie)
+
+            store.searchMovie = "";
+            }
+
+            axios.get(listMovie)
+                .then(response =>{
+                    store.movieList = response.data.results;
+                    console.log(store.movieList)
+                })
+
+
+        }
+    },
+    created(){
+        this.searchFilm();
+    }
 }
 </script>
 <style lang="scss">
