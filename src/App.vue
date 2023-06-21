10be542e3df9ee6d9ffec78c485f7a9c0ae4ec50
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 21:31:00 +0200

    removed the comment on which i copied as a comment my apikey

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,3 @@ import MainFlix from './components/MainFlix.vue';
 @import '../node_modules/bootstrap/scss/bootstrap';
 
 </style>
-
-
-<!--b4e23a9ed230e66a8e63084cff9d0035-->
-<!--
-  https://api.themoviedb.org/3/search/movie?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=
--->
