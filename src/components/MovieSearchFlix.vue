0cf7578c2b9042970245e02efd44e67f68fa6a5f
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 15:13:11 +0200

    created a new component for search bar movie

---
+++ src/components/MovieSearchFlix.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <h1>
+        
+    </h1>
+</template>
+
+
+<script>
+export default {
+    name :'MovieSearchFlix',
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+        
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
