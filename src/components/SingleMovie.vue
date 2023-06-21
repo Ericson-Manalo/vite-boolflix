70b721f2fa5c930f6f97ec5d07fa35b7c334ec90
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 22:08:49 +0200

    called by props the obj and put into the template data requested as title language vote

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -1,5 +1,24 @@
-<template lang="">
-    
+<template>
+    <div>
+        <h1>
+            Title: 
+            {{ infos.title }}
+        </h1>
+        <hr>
+        <p>
+            Original title:
+            {{ infos.original_title }}
+        </p>
+        <p>
+            Original language:
+            {{ infos.original_language }}
+        </p>
+        <p>
+            {{ infos.vote_average }}
+        </p>
+
+    </div>
+
 </template>
 
 
@@ -11,11 +30,17 @@ export default {
             
         }
     },
+
+    props:{
+        infos : Object
+    },
     components:{
         
+    },
+    created(){
     }
 }
 </script>
 <style lang="scss">
-    
+
 </style>
