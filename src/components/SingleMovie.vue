31883499ac73396182f8d5cbe5201f2a60a9599f
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 16:35:58 +0200

    created a single movie component

---
+++ src/components/SingleMovie.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    
+</template>
+
+
+<script>
+export default {
+    name :'SingleMovie',
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+        
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
