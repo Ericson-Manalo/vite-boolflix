c4a0b92c6e7d909faa6fc04f039a9e2faf80e675
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 19:12:08 +0200

    moved all my data to a store so i can share it to other componenets

--- src/components/MainFlix.vue
+++ src/components/MainFlix.vue
@@ -3,48 +3,32 @@
         MainFlix
     </h1>
 
-    <MovieSearchFlix/>
+import MovieList from './MovieList.vue';
 
-    <MovieList :movieList="movieList"/>
+    <MovieSearchFlix @searched/>
+    <MovieList/>
+
 </template>
 
 
 <script>
-import MovieList from './MovieList.vue'
-import MovieSearchFlix from './MovieSearchFlix.vue'
-import axios from 'axios'
+import {store} from '../store.js';
+import MovieSearchFlix from './MovieSearchFlix.vue';
+import MovieList from './MovieList.vue';
+
 
 export default {
     name:'MainFlix',
     data() {
         return {
-            movieList: [],
-            apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=b4e23a9ed230e66a8e63084cff9d0035&query='
+            store
+
         }
     },
     components:{
-        MovieList,
-        MovieSearchFlix
-        
-    },
-
-    methods: {
-        searchFlix(needle = ''){
-            axios.get(this.apiUrl,{
-                params:{
-                    name: needle
-                } 
-            })
-            .then ((response) =>{
-                this.movieList = response.data.results;
-                console.log(this.movieList)
-            })
-        }
-    },
-
-    created() {
-        this.searchFlix();
-    },
+    MovieSearchFlix,
+    MovieList
+}
 }
 </script>
 <style lang="scss">
