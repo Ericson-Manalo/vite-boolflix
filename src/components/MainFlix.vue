f3a8fb75ca8a6e9c69048508d576166155ddd16a
Ericson-Manalo <ericmana98@gmail.com>
2023-06-21 16:50:24 +0200

    created an array and created apiUrl to take on it what data i need

--- src/components/MainFlix.vue
+++ src/components/MainFlix.vue
@@ -10,7 +10,7 @@
 
 
 <script>
-import MovieList from './MoveList.vue'
+import MovieList from './MovieList.vue'
 import MovieSearchFlix from './MovieSearchFlix.vue'
 import axios from 'axios'
 
@@ -18,14 +18,33 @@ export default {
     name:'MainFlix',
     data() {
         return {
-            
+            movieList: [],
+            apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=b4e23a9ed230e66a8e63084cff9d0035&'
         }
     },
     components:{
         MovieList,
         MovieSearchFlix
         
-    }
+    },
+
+    methods: {
+        searchFlix(needle = ''){
+            axios.get(this.apiUrl,{
+                params:{
+                    name:needle
+                } 
+            })
+            .then ((response) =>{
+                this.movieList = response.data;
+                console.log(this.movieList)
+            })
+        }
+    },
+
+    created() {
+        this.searchFlix();
+    },
 }
 </script>
 <style lang="scss">
