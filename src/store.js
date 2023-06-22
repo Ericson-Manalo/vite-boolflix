b0b5a8ea35e89a7b8051c5ff4b275464c4bd9952
Ericson-Manalo <ericmana98@gmail.com>
2023-06-22 18:01:46 +0200

    readded lang in store

--- src/store.js
+++ src/store.js
@@ -7,5 +7,5 @@ export const store = reactive({
     apiSeriesUrl: 'https://api.themoviedb.org/3/search/tv?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=',
     seriesList: [],
     searchMovie: '',
-    
+    lang: ['en', 'es', 'it', 'ja', 'fr']
 })
