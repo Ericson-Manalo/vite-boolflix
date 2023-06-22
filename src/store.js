4a4dc5fe5df2b19c150acf919813fc8767f29f84
Ericson-Manalo <ericmana98@gmail.com>
2023-06-22 11:15:41 +0200

    created an array of language

--- src/store.js
+++ src/store.js
@@ -3,5 +3,6 @@ import {reactive} from 'vue';
 export const store = reactive({
     apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=',
     movieList: [],
-    searchMovie: ''
+    searchMovie: '',
+    lang: ['en', 'es', 'it', 'ja', 'fr']
 })
