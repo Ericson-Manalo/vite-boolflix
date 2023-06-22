5313a6714def6536f1662619442e4fe3bb28c7bc
Ericson-Manalo <ericmana98@gmail.com>
2023-06-22 23:06:57 +0200

    added some comment

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import {reactive} from 'vue';
 
 export const store = reactive({
+    /* API URL MOVIES */
     apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=',
     movieList: [],
 
+    /* API URL MOVIES */
     apiSeriesUrl: 'https://api.themoviedb.org/3/search/tv?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=',
     seriesList: [],
+
     searchMovie: '',
     lang: ['en', 'es', 'it', 'ja', 'fr']
 })
