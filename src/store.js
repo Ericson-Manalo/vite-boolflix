94c7a986e54aad100710e6c48b14262a1ab18a31
Ericson-Manalo <ericmana98@gmail.com>
2023-06-22 12:46:23 +0200

    saved into my store apiseriesUrl and an empty array

--- src/store.js
+++ src/store.js
@@ -3,6 +3,9 @@ import {reactive} from 'vue';
 export const store = reactive({
     apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=',
     movieList: [],
+
+    apiSeriesUrl: 'https://api.themoviedb.org/3/search/tv?api_key=b4e23a9ed230e66a8e63084cff9d0035&query=',
+    seriesList: [],
     searchMovie: '',
     lang: ['en', 'es', 'it', 'ja', 'fr']
 })
