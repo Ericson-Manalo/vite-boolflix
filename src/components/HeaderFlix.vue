ad0a55adf53ffd922fdf19eb6a1d4e137e38602d
Ericson-Manalo <ericmana98@gmail.com>
2023-06-25 17:54:41 +0200

    adding the navbar to my header

--- src/components/HeaderFlix.vue
+++ src/components/HeaderFlix.vue
@@ -4,6 +4,32 @@
             <!--credits to Emanuela Pau-->
             <img src="/img/boolflix_logo.png" alt="Boolflix's logo">
         </div>
+        <nav class="navbar">
+            <ul class="d-flex">
+                <li>
+                    Home
+                </li>
+                <li>
+                    TV Shows
+                </li>
+                <li>
+                    Movies
+                </li>
+                <li>
+                    New & Popular
+                </li>
+                <li>
+                    My List
+                </li>
+                <li>
+                    Browse by Languages
+                </li>
+            </ul>
+        </nav>
+
+        <div>
+
+        </div>
     </header>
 </template>
 
@@ -24,5 +50,25 @@ export default {
 <style lang="scss" scoped>
     header{
         height: 100px;
+        display: flex;
+        color: white;
+    }
+
+    div.logo{
+        height: 100%;
+    }
+
+    nav{
+        display: flex;
+        height: 100%;
+        width: 45%;
+    }
+
+    nav ul{
+        display: flex;
+        align-items: center;
+        width: 100%;
+        justify-content: space-between;
+
     }
 </style>
